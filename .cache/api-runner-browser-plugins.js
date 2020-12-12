90ffa76b6fb462f69d4826027cd33aeac329169f
JP Park <jaepyunp@andrew.cmu.edu>
2020-12-12 09:31:28 +0900

    final
    final

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/profile.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"5d9936e4ed495116d6623e7514ab0972"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-smoothscroll/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
